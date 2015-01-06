d41cf5219dc12646a279f4d4ab0931a0001575ca
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-01-06 12:06:43 +0200

    Adding LogView plugin

---
+++ LogViewPlugin/LogAppender.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using CKAN;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+
+namespace LogViewPlugin
+{
+    class LogAppender : AppenderSkeleton
+    {
+
+        private TextBox m_Textbox;
+
+        public LogAppender(TextBox textbox)
+        {
+            m_Textbox = textbox;
+            PatternLayout layout = new PatternLayout();
+            layout.ConversionPattern = "%newline%date %-5level %logger – %message – %property%newline";
+            layout.ActivateOptions();
+            Layout = layout;
+            ActivateOptions();
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            var message = RenderLoggingEvent(loggingEvent);
+            Util.Invoke(m_Textbox, () => m_Textbox.AppendText(message));
+        }
+    }
+}
---
+++ LogViewPlugin/LogViewPlugin.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+using CKAN;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
+
+namespace LogViewPlugin
+{
+
+    public class LogViewPlugin : CKAN.IGUIPlugin
+    {
+
+        private readonly CKAN.Version VERSION = new CKAN.Version("v1.0.0");
+
+        public override void Initialize()
+        {
+            var tabPage = new TabPage();
+            tabPage.Name = "LogViewTabPage";
+            tabPage.Text = "LogView";
+
+            var textbox = new TextBox();
+            textbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            textbox.Multiline = true;
+            textbox.Name = "LogViewTextBox";
+            textbox.ReadOnly = true;
+
+            tabPage.Controls.Add(textbox);
+
+            Main.Instance.m_TabController.m_TabPages.Add("LogViewTabPage", tabPage);
+            Main.Instance.m_TabController.ShowTab("LogViewTabPage", 1, false);
+
+            Hierarchy h = (Hierarchy)LogManager.GetRepository();
+            h.Root.Level = Level.All;
+
+            IAppender appender = new LogAppender(textbox);
+            h.Root.AddAppender(appender);
+        }
+
+        public override void Deinitialize()
+        {
+            Main.Instance.m_TabController.HideTab("LogViewTabPage");
+            Main.Instance.m_TabController.m_TabPages.Remove("LogViewTabPage");
+        }
+
+        public override string GetName()
+        {
+            return "LogView";
+        }
+
+        public override CKAN.Version GetVersion()
+        {
+            return VERSION;
+        }
+
+    }
+
+}
